cb4e1aab8d82c6e1a1cf7265065454529bea2047
DungGramer <dung.dev.gramer@gmail.com>
2022-09-26 00:00:46 +0700

    Add code highlight

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,6 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "highlight.js/styles/atom-one-dark.css";
+import "styles/index.scss";
 
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'styles/index.scss';
-
-export default ({Component, pageProps}) => <Component {...pageProps} />
+export default ({ Component, pageProps }) => <Component {...pageProps} />;
