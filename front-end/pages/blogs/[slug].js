6809dbaffd637ed70e36f03d4ce50eea188d5f2b
DungGramer <dung.dev.gramer@gmail.com>
2022-09-25 22:58:14 +0700

    Get blog data

--- front-end/pages/blogs/[slug].js
+++ front-end/pages/blogs/[slug].js
@@ -1,15 +1,25 @@
 import PageLayout from "components/PageLayout";
+import { getBlogBySlug } from "lib/api";
 import { useRouter } from "next/router";
 
-const BlogDetail = () => {
+const BlogDetail = ({ blog }) => {
   const { query } = useRouter();
 
   return (
     <PageLayout>
       <h1>Blog Detail</h1>
-      <p>Blog slug: {query.slug}</p>
+      <p>Blog slug: {blog?.slug}</p>
     </PageLayout>
   );
 };
+
+export async function getServerSideProps({ params }) {
+  const blog = await getBlogBySlug(params.slug);
+  return {
+    props: {
+      blog,
+    },
+  };
+}
 
 export default BlogDetail;
